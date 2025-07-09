cbeb3c925def98233e74b2640a021b521dcc736f
swetha4444 <swethasaseendran4@gmail.com>
2025-07-08 22:56:50 -0400

    feat: Add audio background to layout with MatrixRain effect

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import MatrixRain from "../backgrounds/matrixRain";
 import NavBar from "./navbar";
 
 export default function Layout({ children }) {
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0.2;
+      audioRef.current.play().catch(() => {});
+    }
+  }, []);
+
   return (
     <div className="relative min-h-screen bg-black overflow-hidden z-0">
       <MatrixRain />
+      <audio
+        ref={audioRef}
+        src={process.env.PUBLIC_URL + "/music/matrix-bg.mp3"}
+        autoPlay
+        loop
+        controls={false}
+        style={{ display: "none" }}
+      />
       {children}
     </div>
   );
