b4f522b8614c9083843c1cc81bf627c934a5aaec
swetha4444 <swethasaseendran4@gmail.com>
2025-07-08 23:26:05 -0400

    feat: Remove audio volume setting and adjust layout structure

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,9 +6,6 @@ export default function Layout({ children }) {
   const audioRef = useRef(null);
 
   useEffect(() => {
-    if (audioRef.current) {
-      audioRef.current.volume = 0.2; // Set volume to 20%
-    }
     const playAudio = () => {
       if (audioRef.current) {
         audioRef.current.play().catch(() => {});
@@ -22,6 +19,7 @@ export default function Layout({ children }) {
   return (
     <div className="relative min-h-screen bg-black overflow-hidden z-0">
       <MatrixRain />
+      <NavBar />
       <audio
         ref={audioRef}
         src={process.env.PUBLIC_URL + "/music/matrix-bg.mp3"}
