55899de3e9e5a2584f374d87dacd3e39a1004fe2
swetha4444 <swethasaseendran4@gmail.com>
2025-07-08 23:26:27 -0400

    feat: Remove NavBar component from Layout for cleaner design

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from "react";
 import MatrixRain from "../backgrounds/matrixRain";
-import NavBar from "./navbar";
 
 export default function Layout({ children }) {
   const audioRef = useRef(null);
@@ -19,7 +18,6 @@ export default function Layout({ children }) {
   return (
     <div className="relative min-h-screen bg-black overflow-hidden z-0">
       <MatrixRain />
-      <NavBar />
       <audio
         ref={audioRef}
         src={process.env.PUBLIC_URL + "/music/matrix-bg.mp3"}
